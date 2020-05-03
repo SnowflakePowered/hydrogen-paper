b62cde1eca4e70eece6f8ee8ae1be1ca645064ab
Ronny Chan <ronny6993@gmail.com>
2020-05-03 02:56:42 -0400

    GamePlayButtonHover: prevent double event on button click

--- theme/src/components/GamePlayButtonHover/GamePlayButtonHover.tsx
+++ theme/src/components/GamePlayButtonHover/GamePlayButtonHover.tsx
@@ -3,16 +3,23 @@ import GamePlayButton from 'components/GamePlayButton/GamePlayButton'
 import { styles, StyleProps } from './GamePlayButtonHover.style'
 import { withStyles } from '@material-ui/core'
 
+type MouseEventHandler = (event: React.MouseEvent<{}>) => void
+
 type GamePlayButtonProps = {
-    onClick?: (event: React.MouseEvent<{}>) => void,
-    onButtonClick?: (event: React.MouseEvent<{}>) => void,
+    onClick?: MouseEventHandler,
+    onButtonClick?: MouseEventHandler,
     loading?: boolean
-  }
+}
+
+const onlyButtonEvent : (onButtonClick?: MouseEventHandler) => (event: React.MouseEvent<{}>) => void = (onButtonClick) => event => {
+  if (onButtonClick !== undefined) onButtonClick(event)
+  event.stopPropagation()
+}
 
 const GamePlayButtonHover: React.SFC<GamePlayButtonProps & StyleProps> = ({classes, onClick, onButtonClick, loading}) => (
     <div className={classes.hoverContainer} onClick={onClick}>
       <div className={classes.playButton}>
-          <GamePlayButton onClick={onButtonClick}/>
+          <GamePlayButton onClick={onlyButtonEvent(onButtonClick)}/>
       </div>
     </div>
   )
