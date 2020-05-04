051051f39cffa3d5f8c546f630231eb6a2964b10
Ronny Chan <ronny6993@gmail.com>
2020-05-04 17:15:59 -0400

    NavigationButtons: fix edge

--- theme/src/components/NavigationButtons/NavigationButtons.tsx
+++ theme/src/components/NavigationButtons/NavigationButtons.tsx
@@ -15,10 +15,10 @@ const NavigationButtons: React.FunctionComponent<ComponentProps & StyleProps> =
       [classes.buttonContainer]: true,
       [className ?? '']: className !== undefined
     })}>
-      <IconButton edge="end" onClick={onBackward} className={classes.buttons}>
+      <IconButton onClick={onBackward} className={classes.buttons}>
         <ArrowBack />
       </IconButton>
-      <IconButton edge="end" onClick={onForward} className={classes.buttons}>
+      <IconButton onClick={onForward} className={classes.buttons}>
         <ArrowForward />
       </IconButton>
     </div>
